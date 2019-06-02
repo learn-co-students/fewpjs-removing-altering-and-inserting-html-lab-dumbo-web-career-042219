dc5644f04657eabad4d79ff90699be692dc16051
Aaron Chu <aaronchu357@gmail.com>
2019-06-02 18:30:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("main").remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", 'victory')
+newHeader.innerHTML = "Aaron is the champion!"
