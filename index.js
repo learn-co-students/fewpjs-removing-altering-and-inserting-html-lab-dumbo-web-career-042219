881a22861029def74368eef1f956257951ea2191
askoenig <askoenig94@gmail.com>
2019-06-03 14:22:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.getElementById('main');
+element.remove();
+let newHeader = document.createElement('h1');
+newHeader.innerHTML = 'victory';
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+newHeader.innerHTML= 'Andrew is the champion';
